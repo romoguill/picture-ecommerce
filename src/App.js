acc580fac90d0c6112ed9ee227ceaf7f5c1b1b1a
romoguill <romoguill@gmail.com>
2022-07-30 10:29:24 -0300

    Setup React Router with routes to home and cart

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import Header from './components/Header';
 import Cart from './pages/Cart';
 import Photos from './pages/Photos';
 
+import './styles.css';
+
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <Header />
       <h1>Home Page</h1>
+
+      <Routes>
+        <Route path="/" element={<Photos />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
