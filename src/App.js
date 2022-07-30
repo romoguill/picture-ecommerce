fd5d492cb79f286687225e9b2e3acf7938b70ef2
romoguill <romoguill@gmail.com>
2022-07-30 09:55:03 -0300

    Setup basic components and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Header from './components/Header';
+import Cart from './pages/Cart';
+import Photos from './pages/Photos';
+
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="App">
+      <Header />
+      <h1>Home Page</h1>
+    </div>
+  );
 }
 
 export default App;
