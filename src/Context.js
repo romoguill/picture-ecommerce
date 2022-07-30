7de32fb6fa36ff2ab10cd841e6a6b87e51b37bdf
romoguill <romoguill@gmail.com>
2022-07-30 10:59:56 -0300

    Created a Context Component

---
+++ src/Context.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Context = React.createContext();
+
+function ContextProvider(props) {
+  return <Context.Provider value={''}>{props.children}</Context.Provider>;
+}
+
+export { Context, ContextProvider };
